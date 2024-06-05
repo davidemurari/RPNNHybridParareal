f2d403d8e8240ad50d4b4a6203efcae1a3d28768
Davide Murari <53520360+davidemurari@users.noreply.github.com>
2024-06-05 14:14:01 +0200

    First commit with the code for the paper

---
+++ ELM/scripts/plotting.py
@@ -0,0 +1,368 @@
+def plot_results(system,time_plot,time_plot_sequential,output,network_sol,list_of_labels,n_x,L,vec,avg_time=-1,number_iterates=1,btype=None,network_sol_lobatto=[],avg_time_lobatto=None,network_sol_SIR_flow=[],avg_time_SIR_flow=None):
+
+    import numpy as np
+    import matplotlib.pyplot as plt
+    import time as time_lib
+    import os
+    import matplotlib
+    from matplotlib.ticker import FormatStrFormatter
+    from mpl_toolkits.mplot3d import Axes3D
+
+    #Setting the plotting parameters
+    matplotlib.rcParams['text.usetex'] = True
+    matplotlib.rcParams['text.latex.preamble'] = r'\usepackage{amsmath}'
+    matplotlib.rcParams['font.size']= 45
+    matplotlib.rcParams['font.family']= 'ptm' #'Times New Roman
+
+    if not os.path.exists("savedPlots/"):
+        os.mkdir("savedPlots")
+
+    if system=="Rober":
+        
+        fact = 1e4
+        fig = plt.figure(figsize=(20,10))
+        back_colors = ["k","b","darkgreen","darkslategrey"]
+        front_colors = ["r","plum","darkturquoise","lightsalmon"]
+
+        for i in range(len(network_sol)):
+            if i==1:
+                plt.plot(time_plot_sequential,output[:,i]*fact,'-',color=back_colors[i],label=f"{list_of_labels[i]} ref",linewidth=5)
+                plt.plot(time_plot,network_sol[i]*fact,'--',color=front_colors[i],label=f"{list_of_labels[i]} para",linewidth=5)
+            else:
+                plt.plot(time_plot_sequential,output[:,i],'-',color=back_colors[i],label=f"{list_of_labels[i]} ref",linewidth=5)
+                plt.plot(time_plot,network_sol[i],'--',color=front_colors[i],label=f"{list_of_labels[i]} para",linewidth=5)
+
+        if avg_time==-1:
+            pass #title = f"Comparison of solutions"
+        else:
+            title = f"Average computational time: {np.round(avg_time,2)}s" if number_iterates>1 \
+                            else f"Computational time: {np.round(avg_time,2)}s"
+                
+            plt.title(title)
+        plt.xlabel(r'$t$')
+        plt.legend(loc='center', bbox_to_anchor=(1.14,0.5))
+
+        #if avg_time!=-1:
+        plt.savefig(f"savedPlots/{system}.pdf",bbox_inches='tight')
+        plt.show();
+
+    if system=="SIR":
+        
+        if len(network_sol_SIR_flow)==0:
+            fig = plt.figure(figsize=(30,10))
+            
+            
+            back_colors = ["k","b","darkgreen","darkslategrey"]
+            front_colors = ["r","plum","darkturquoise","lightsalmon"]
+            
+            for i in range(len(network_sol)):
+                plt.plot(time_plot_sequential,output[:,i],'-',color=back_colors[i],label=f"{list_of_labels[i]} ref",linewidth=5)
+                plt.plot(time_plot,network_sol[i],'--',color=front_colors[i],label=f"{list_of_labels[i]} para",linewidth=5)
+            
+            plt.xlabel(r'$t$')
+            
+            plt.ylim(bottom=-0.1,top=1.75)
+            plt.legend(loc='upper center', ncol=2)
+            
+            if avg_time==-1:
+                pass #title = f"Comparison of solutions,\n ELM"
+            else:
+                title = f"ELM, Average computational time: {np.round(avg_time,2)}s"
+                plt.title(title)
+
+            #if avg_time!=-1:
+            plt.savefig(f"savedPlots/{system}.pdf",bbox_inches='tight')
+            plt.show();
+        else:
+            fig = plt.figure(figsize=(30,10))
+            ax1 = plt.subplot2grid((1, 30), (0, 0), colspan=14)
+                        
+            back_colors = ["k","b","darkgreen","darkslategrey"]
+            front_colors = ["r","plum","darkturquoise","lightsalmon"]
+            
+            for i in range(len(network_sol)):
+                ax1.plot(time_plot_sequential,output[:,i],'-',color=back_colors[i],label=f"{list_of_labels[i]} ref",linewidth=5)
+                ax1.plot(time_plot,network_sol[i],'--',color=front_colors[i],label=f"{list_of_labels[i]} para",linewidth=5)
+            
+            if avg_time==-1:
+                ax1.set_title(f"ELM")
+            else:
+                ax1.set_title(f"ELM \n Average computational time: {np.round(avg_time,2)}s")
+            ax1.set_xlabel(r'$t$')
+            
+            ax1.set_ylim(bottom=-0.1,top=1.75)
+            ax1.legend(loc='upper center', ncol=2)
+            
+            ax2 = plt.subplot2grid((1, 30), (0, 16), colspan=15)
+            
+            for i in range(len(network_sol_SIR_flow)):
+                ax2.plot(time_plot_sequential,output[:,i],'-',color=back_colors[i],label=f"{list_of_labels[i]} ref",linewidth=5)
+                ax2.plot(time_plot,network_sol_SIR_flow[i],'--',color=front_colors[i],label=f"{list_of_labels[i]} para",linewidth=5)
+            
+            ax2.set_xlabel(r'$t$')
+            
+            ax2.set_ylim(bottom=-0.1,top=1.75)
+            ax2.legend(loc='upper center', ncol=2)
+
+            if avg_time==-1:
+                pass #ax2.set_title(f"Flow map approach")
+            else:
+                ax2.set_title(f"Flow map approach \n Average computational time: {np.round(avg_time_SIR_flow,2)}s")
+                #fig.suptitle(title)
+            plt.subplots_adjust(top=0.76, wspace=0.5)
+
+            #if avg_time!=-1:
+            plt.savefig(f"savedPlots/{system}.pdf",bbox_inches='tight')
+            plt.show();
+    
+    if system=="Lorenz":
+        
+        fig = plt.figure(figsize=(30,10))
+        ax1 = plt.subplot2grid((1, 30), (0, 0), colspan=14)
+                
+        back_colors = ["k","b","darkgreen","darkslategrey"]
+        front_colors = ["r","plum","darkturquoise","lightsalmon"]
+        
+        for i in range(len(network_sol)):
+            ax1.plot(time_plot_sequential,output[:,i],'-',color=back_colors[i],label=f"{list_of_labels[i]} ref",linewidth=5)
+            ax1.plot(time_plot,network_sol[i],'--',color=front_colors[i],label=f"{list_of_labels[i]} para",linewidth=5)
+        
+        if avg_time==-1:
+            ax1.set_title(f"Uniform nodes")
+        else:
+            ax1.set_title(f"Uniform nodes \n Average computational time: {np.round(avg_time,2)}s")
+        ax1.set_xlabel(r'$t$')
+        
+        ax1.set_ylim(bottom=-30.,top=120.)
+        ax1.legend(loc='upper center', ncol=2)
+        
+        ax2 = plt.subplot2grid((1, 30), (0, 16), colspan=15)
+        
+        for i in range(len(network_sol)):
+            ax2.plot(time_plot_sequential,output[:,i],'-',color=back_colors[i],label=f"{list_of_labels[i]} ref",linewidth=5)
+            ax2.plot(time_plot,network_sol_lobatto[i],'--',color=front_colors[i],label=f"{list_of_labels[i]} para",linewidth=5)
+        
+        ax2.set_xlabel(r'$t$')
+        
+        ax2.set_ylim(bottom=-30.,top=120.)
+        ax2.legend(loc='upper center', ncol=2)
+
+        if avg_time==-1:
+            ax2.set_title(f"Lobatto nodes")
+        else:
+            ax2.set_title(f"Lobatto nodes \n Average computational time: {np.round(avg_time_lobatto,2)}s")
+        plt.subplots_adjust(top=0.76, wspace=0.5)
+
+        #if avg_time!=-1:
+        plt.savefig(f"savedPlots/{system}.pdf",bbox_inches='tight')
+        plt.show();
+        
+    if system=="Brusselator":
+        
+        fig = plt.figure(figsize=(30,10))
+        ax1 = plt.subplot2grid((1, 30), (0, 0), colspan=20)
+        
+        back_colors = ["k","b","darkgreen","darkslategrey"]
+        front_colors = ["r","plum","darkturquoise","lightsalmon"]
+        
+        for i in range(len(network_sol)):
+            ax1.plot(time_plot_sequential,output[:,i],'-',color=back_colors[i],label=f"{list_of_labels[i]} ref",linewidth=5)
+            ax1.plot(time_plot,network_sol[i],'--',color=front_colors[i],label=f"{list_of_labels[i]} para",linewidth=5)
+        
+        ax1.set_ylim(-2,max(network_sol.reshape(-1))+0.1)
+        ax1.set_xlabel(r'$t$')
+        
+        ax1.legend(loc='lower center', ncol=2)
+        
+        ax2 = plt.subplot2grid((1, 30), (0, 22), colspan=8)
+        
+        ax2.plot(output[:,0],output[:,1],'k-',label="ref",linewidth=5)
+        ax2.plot(network_sol[0],network_sol[1],'r--',label="para",linewidth=5)
+        
+        ax2.set_xlabel(list_of_labels[0])
+        ax2.set_ylabel(list_of_labels[1])
+        ax2.legend(loc='lower center', ncol=1)
+        ax2.set_ylim(-1.,max(network_sol.reshape(-1))+0.1)
+        
+        if avg_time==-1:
+            pass #title = f"Comparison of solutions"
+        else:
+            title = f"Average computational time: {np.round(avg_time,2)}s" if number_iterates>1 \
+                    else f"Computational time: {np.round(avg_time,2)}s"
+            fig.suptitle(title)
+        
+        plt.subplots_adjust(top=0.9, wspace=0.5)
+
+        #if avg_time!=-1:
+        plt.savefig(f"savedPlots/{system}.pdf",bbox_inches='tight')
+        plt.show();
+        
+    if system=="Arenstorf":
+        
+        fig = plt.figure(figsize=(30,10))
+        ax1 = plt.subplot2grid((1, 30), (0, 0), colspan=20)
+        
+        back_colors = ["k","b","darkgreen","darkslategrey"]
+        front_colors = ["r","plum","darkturquoise","lightsalmon"]
+        
+        for i in range(len(network_sol)):
+            ax1.plot(time_plot_sequential,output[:,i],'-',color=back_colors[i],label=f"{list_of_labels[i]} ref",linewidth=5)
+            ax1.plot(time_plot,network_sol[i],'--',color=front_colors[i],label=f"{list_of_labels[i]} para",linewidth=5)
+        
+        ax1.set_ylim(-4.5,1.5)
+        ax1.set_xlabel(r'$t$')
+        
+        ax1.legend(loc='lower center', ncol=3)
+        
+        ax2 = plt.subplot2grid((1, 30), (0, 22), colspan=8)
+        
+        ax2.plot(output[:,0],output[:,2],'k-',label="ref",linewidth=5)
+        ax2.plot(network_sol[0],network_sol[2],'r--',label="para",linewidth=5)
+        
+        ax2.set_xlabel(list_of_labels[0])
+        ax2.set_ylabel(list_of_labels[2])
+        ax2.legend(loc='lower center', ncol=1)
+        ax2.set_ylim(-2.5,1.5)
+        if avg_time==-1:
+            pass #title = f"Comparison of solutions"
+        else:
+            title = f"Average computational time: {np.round(avg_time,2)}s" if number_iterates>1 \
+                    else f"Computational time: {np.round(avg_time,2)}s"
+        
+            fig.suptitle(title)
+        plt.subplots_adjust(top=0.9, wspace=0.5)
+
+        #if avg_time!=-1:
+        plt.savefig(f"savedPlots/{system}.pdf",bbox_inches='tight')
+        plt.show();
+
+    if system=="Burger":
+
+        '''fig = plt.figure(figsize=(40, 13))
+        ax1 = plt.subplot2grid((1, 40), (0, 0), colspan=20)
+
+        step = len(time_plot) // 10
+        for i in range(0, len(time_plot), step):
+            if i == 0:
+                ax1.plot(vec.x, output[i], 'k-', linewidth=5, label="ref")
+                ax1.plot(vec.x, network_sol[:, i], 'r--', linewidth=5, label="para")
+            else:
+                ax1.plot(vec.x, output[i], 'k-', linewidth=5)
+                ax1.plot(vec.x, network_sol[:, i], 'r--', linewidth=5)
+
+        ax1.set_title("Solution \n snapshots")#,fontsize=45)
+        ax1.set_xlabel(r'$x$')#,fontsize=45)
+        ax1.set_ylabel(r'$u(x,t)$')#,fontsize=45)
+        ax1.legend(loc='upper right', frameon=True, shadow=True)
+
+        ax2 = plt.subplot2grid((1, 40), (0, 20), colspan=9, projection='3d')  # 1 row, 2 columns, second subplot
+        ax3 = plt.subplot2grid((1, 40), (0, 30), colspan=9, projection='3d')  # 1 row, 2 columns, second subplot
+        
+
+        T, X = np.meshgrid(time_plot, vec.x)
+        ax2.plot_surface(X, T, output.T, cmap='viridis', edgecolor='none')
+        ax3.plot_surface(X, T, network_sol, cmap='viridis', edgecolor='none')
+        ax2.set_title("Reference \n solution",fontsize=45)
+        ax3.set_title("Network \n solution",fontsize=45)
+        ax2.set_xlabel(r'$x$',fontsize=45)
+        ax2.set_ylabel(r'$t$',fontsize=45)
+        ax3.set_xlabel(r'$x$',fontsize=45)
+        ax3.set_ylabel(r'$t$',fontsize=45)
+        
+        ax2.tick_params(axis='x', labelrotation=55)
+        ax2.set_xticks([0.0,1.0])
+        ax2.tick_params(axis='y', labelrotation=-25)
+        ax2.set_yticks([0.0,1.0])
+        ax2.tick_params(axis='z', pad=20)
+        
+        ax3.tick_params(axis='x', labelrotation=55)
+        ax3.set_xticks([0.0,1.0])
+        ax3.tick_params(axis='y', labelrotation=-25)
+        ax3.set_yticks([0.0,1.0])
+        ax3.tick_params(axis='z', pad=20)
+
+        ax2.xaxis.set_major_formatter(FormatStrFormatter('%.1f'))
+        ax3.xaxis.set_major_formatter(FormatStrFormatter('%.1f'))
+        
+        ax2.yaxis.set_major_formatter(FormatStrFormatter('%.1f'))
+        ax3.yaxis.set_major_formatter(FormatStrFormatter('%.1f'))
+
+        if avg_time==-1:
+            pass #fig.suptitle(f"Comparison of solutions")
+        else:
+            fig.suptitle(f"Average computational time: {np.round(avg_time,2)}s" if number_iterates > 1 \
+                        else f"Computational time: {np.round(avg_time,2)}s")#,fontsize=45)
+
+        plt.subplots_adjust(top=0.8, wspace=0.5)
+
+        #if avg_time!=-1:
+        plt.savefig(f"savedPlots/{system}_{btype}.pdf", bbox_inches='tight')
+        plt.show()'''
+        import matplotlib.pyplot as plt
+        import numpy as np
+        from matplotlib.ticker import FormatStrFormatter
+
+        fig = plt.figure(figsize=(40, 13))
+        ax1 = plt.subplot2grid((1, 40), (0, 0), colspan=20)
+
+        step = len(time_plot) // 10
+        for i in range(0, len(time_plot), step):
+            if i == 0:
+                ax1.plot(vec.x, output[i], 'k-', linewidth=5, label="ref")
+                ax1.plot(vec.x, network_sol[:, i], 'r--', linewidth=5, label="para")
+            else:
+                ax1.plot(vec.x, output[i], 'k-', linewidth=5)
+                ax1.plot(vec.x, network_sol[:, i], 'r--', linewidth=5)
+
+        ax1.set_title("Solution \n snapshots")#,fontsize=45)
+        ax1.set_xlabel(r'$x$')#,fontsize=45)
+        ax1.set_ylabel(r'$u(x,t)$')#,fontsize=45)
+        ax1.legend(loc='upper right', frameon=True, shadow=True)
+
+        ax2 = plt.subplot2grid((1, 40), (0, 20), colspan=9, projection='3d')  # 1 row, 2 columns, second subplot
+        ax3 = plt.subplot2grid((1, 40), (0, 30), colspan=9, projection='3d')  # 1 row, 2 columns, second subplot
+
+        T, X = np.meshgrid(time_plot, vec.x)
+        ax2.plot_surface(X, T, output.T, cmap='viridis', edgecolor='none')
+        ax3.plot_surface(X, T, network_sol, cmap='viridis', edgecolor='none')
+        ax2.set_title("Reference \n solution",fontsize=45)
+        ax3.set_title("Network \n solution",fontsize=45)
+        ax2.set_xlabel(r'$x$',fontsize=45)
+        ax2.set_ylabel(r'$t$',fontsize=45)
+        ax3.set_xlabel(r'$x$',fontsize=45)
+        ax3.set_ylabel(r'$t$',fontsize=45)
+
+        ax2.tick_params(axis='x', labelrotation=55)
+        ax2.set_xticks([0.0,1.0])
+        ax2.tick_params(axis='y', labelrotation=-25)
+        ax2.set_yticks([0.0,1.0])
+        ax2.tick_params(axis='z', pad=20)
+
+        ax3.tick_params(axis='x', labelrotation=55)
+        ax3.set_xticks([0.0,1.0])
+        ax3.tick_params(axis='y', labelrotation=-25)
+        ax3.set_yticks([0.0,1.0])
+        ax3.tick_params(axis='z', pad=20)
+
+        ax2.xaxis.set_major_formatter(FormatStrFormatter('%.1f'))
+        ax3.xaxis.set_major_formatter(FormatStrFormatter('%.1f'))
+
+        ax2.yaxis.set_major_formatter(FormatStrFormatter('%.1f'))
+        ax3.yaxis.set_major_formatter(FormatStrFormatter('%.1f'))
+
+        # Adding annotations (a) and (b)
+        fig.text(0.3, -0.05, '(a)', fontweight='bold', ha='center', transform=fig.transFigure)
+        fig.text(0.7, -0.05, '(b)', fontweight='bold', ha='center', transform=fig.transFigure)
+
+        if avg_time==-1:
+            pass #fig.suptitle(f"Comparison of solutions")
+        else:
+            fig.suptitle(f"Average computational time: {np.round(avg_time,2)}s" if number_iterates > 1 \
+                        else f"Computational time: {np.round(avg_time,2)}s")#,fontsize=45)
+
+        plt.subplots_adjust(top=0.8, wspace=0.5)
+
+        #if avg_time!=-1:
+        plt.savefig(f"savedPlots/{system}_{btype}.pdf", bbox_inches='tight')
+        plt.show()
+
---
+++ ELM/scripts/repeated_experiments.py
@@ -0,0 +1,131 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import time as time_lib
+import random
+import os
+
+from tqdm import tqdm
+
+import multiprocessing
+
+from scripts.dynamics import vecField
+from scripts.parareal import parallel_solver
+
+def run_experiment(args,return_nets=False,verbose=False):
+        
+        system, nodes = args
+        if system=="BurgerQ" or system=="Burger":
+                system="Burger"
+                ic = "quadratic"
+        elif system=="Burger1W":
+                system="Burger"
+                ic = "single_wave"
+        elif system=="BurgerSW":
+                system="Burger"
+                ic = "sum_of_waves"
+        
+        vecRef = vecField(system)
+        number_processors = 5 if system=="Rober" or system=="Burger" else 1
+        
+        if system=="BurgerQ" or system=="Burger" or system=="Burger1W" or system=="BurgerSW":
+                n_x = 5#3
+                L = 5#3
+        else:
+                n_x = 5
+                L = 5
+        n_t = 2 #we do all the experiments with n_t = 2, which means we really just have coarse intervals
+                #we do not further split them to simplify the problem. On the other hand, the code is flexible
+                #also to this additional splitting.
+
+        LB = -1.
+        UB = 1.
+
+        if system=="Rober":
+                t_max = 100.
+                num_t = 101
+                #L = 5
+                vecRef.dt_fine = 1e-4
+        elif system=="SIR":
+                t_max = 100.
+                num_t = 101
+                #L = 3
+                vecRef.dt_fine = 1e-2
+        elif system=="Brusselator":
+                t_max = 12.
+                num_t = 33
+                #L = 5
+                vecRef.dt_fine = t_max/640
+        elif system=="Arenstorf":
+                t_max = 17 #17.0652165601579625588917206249 #One period
+                num_t = 126
+                #L = 3
+                vecRef.dt_fine = t_max / 80000
+        elif system=="Lorenz":
+                t_max = 10.
+                num_t = 251
+                #L = 3
+                vecRef.dt_fine = t_max / 14500
+        elif system=="Burger":
+                t_max = 1.
+                num_t = 51
+                #L = 3
+                vecRef.dt_fine = t_max / 500
+        else:
+                print("Dynamics not implemented")
+        
+        if system=="Rober":
+                time = np.concatenate((np.linspace(0,1,101)[:-1],np.linspace(1,t_max,34)))
+        else:
+                time = np.linspace(0,t_max,num_t)
+                
+        dts = np.diff(time)
+        
+        if system=="Rober":
+                y0 = np.array([1.,0.,0])
+        elif system=="SIR":
+                y0 = np.array([0.3,0.5,0.2])
+        elif system=="Brusselator":
+                y0 = np.array([0.,1.])
+        elif system=="Arenstorf":
+                y0 = np.array([0.994,0,0.,-2.00158510637908252240537862224])
+        elif system=="Lorenz":
+                y0 = np.array([20.,5,-5])
+        elif system=="Burger":
+                if ic=="quadratic":
+                        y0 = vecRef.x*(1.-vecRef.x)
+                elif ic=="single_wave":
+                        y0 = np.sin(2*np.pi*vecRef.x)
+                else:
+                        y0 = np.sin(2*np.pi*vecRef.x) + np.cos(4*np.pi*vecRef.x) - np.cos(8*np.pi*vecRef.x) 
+                
+        else:
+                print("Dynamics not implemented")
+                
+
+        weight = np.random.uniform(low=LB,high=UB,size=(L))
+        bias = np.random.uniform(low=LB,high=UB,size=(L))
+
+        data = {"vecRef":vecRef,
+                "LB" : LB,
+                "time" : time,
+                "dts" : dts,
+                "UB" : UB,
+                "L" : L,
+                "y0" : y0,
+                "n_x" : n_x,
+                "n_t" : n_t,
+                "num_t" : num_t,
+                "system" : system,
+                "nodes" : nodes,
+                "act_name" : "tanh",
+                "number_processors":number_processors,
+                "t_max":t_max,
+                "weight":weight,
+                "bias":bias}
+                
+        coarse_approx,networks,total_time,_,avg_coarse_step = parallel_solver(time,data,dts,vecRef,number_processors,verbose=verbose)
+
+        if return_nets:
+                return total_time,avg_coarse_step,coarse_approx,networks,data
+        else:
+                return total_time, avg_coarse_step
